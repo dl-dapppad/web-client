bcc6534d4b8817273fbcd7d0d284caf1c467846b
Nazar Revom <shevanzr@gmail.com>
2022-11-04 15:37:02 +0100

    Added CommonForm, added isAddress to validators

--- src/validators.ts
+++ src/validators.ts
@@ -1,6 +1,7 @@
 import {
   required as _required,
   email as _email,
+  numeric as _numeric,
   minLength as _minLength,
   maxLength as _maxLength,
   sameAs as _sameAs,
@@ -10,6 +11,7 @@ import { Ref } from 'vue'
 import { createI18nMessage, MessageProps } from '@vuelidate/validators'
 import { get } from 'lodash-es'
 import { i18n } from '@/localization'
+import { ethers } from 'ethers'
 
 const { t } = i18n.global || i18n
 
@@ -21,6 +23,15 @@ const withI18nMessage = createI18nMessage({ t, messagePath })
 export const required = <ValidationRule>withI18nMessage(_required)
 
 export const email = <ValidationRule>withI18nMessage(_email)
+
+export const isAddress = <ValidationRule>withI18nMessage({
+  $validator: (address: string) => ethers.utils.isAddress(address),
+  $params: {
+    type: 'isAddress',
+  },
+})
+
+export const numeric = <ValidationRule>withI18nMessage(_numeric)
 
 export const minLength = (length: number): ValidationRule =>
   <ValidationRule>withI18nMessage(_minLength(length))
