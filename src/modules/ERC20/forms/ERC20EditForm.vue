1299a9d8d2ccd586c861a08a3f037194853c3c54
Oleksandr Fedorenko <oleksandr.fedorenko@distributedlab.com>
2022-11-05 06:11:06 +0000

    fix(): after rebase

--- src/modules/ERC20/forms/ERC20EditForm.vue
+++ src/modules/ERC20/forms/ERC20EditForm.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { AppBlock, AppButton, Tabs } from '@/common'
-import { InputField } from '@/fields'
+import { CommonForm } from '@/forms'
 import {
   Bus,
   cropAddress,
@@ -14,6 +14,7 @@ import {
 import { useProductErc20 } from '@/modules/ERC20/composables/use-product-erc20'
 import { useWeb3ProvidersStore } from '@/store'
 import { storeToRefs } from 'pinia'
+import { required, isAddress, numeric } from '@/validators'
 
 const { provider } = storeToRefs(useWeb3ProvidersStore())
 
@@ -90,26 +91,6 @@ const init = async () => {
 
 init()
 
-const approveForm = reactive({
-  spenderAddress: '',
-  valueAmount: '',
-})
-
-const transferFromForm = reactive({
-  fromAddress: '',
-  toAddress: '',
-  valueAmount: '',
-})
-
-const transferForm = reactive({
-  toAddress: '',
-  valueAmount: '',
-})
-
-const transferOwnershipForm = reactive({
-  newOwnerAddress: '',
-})
-
 const handleApprove = () => {
   try {
     Bus.success('approve')
@@ -138,6 +119,149 @@ const formScheme = {
 </script>
 
 <template>
+  <div class="erc20-edit-form">
+    <div class="erc20-edit-form__titles-wrp">
+      <div class="app__module-title-wrp">
+        <app-button
+          type="button"
+          class="app__module-back-btn"
+          :icon-right="$icons.arrowLeft"
+          modification="border-circle"
+          color="tertiary"
+          @click="router.go(-1)"
+        />
+        <h2 class="app__module-title">
+          {{ t('title') }}
+        </h2>
+        <app-button
+          type="button"
+          class="app__module-title-address"
+          :text="
+            t('address', {
+              address: cropAddress(
+                '0xC87B0398F86276D3D590A14AB53fF57185899C42',
+              ),
+            })
+          "
+          :icon-right="$icons.duplicate"
+          scheme="default"
+          size="default"
+          @click="copyToClipboard('0xC87B0398F86276D3D590A14AB53fF57185899C42')"
+        />
+      </div>
+      <span class="app__module-subtitle">
+        {{ t('subtitle') }}
+      </span>
+    </div>
+    <div class="erc20-edit-form__block-wrp">
+      <h3 class="app__module-block-title">
+        {{ t('block-title-1') }}
+      </h3>
+      <app-block>
+        <div class="app__module-content">
+          <div class="app__metadata erc20-edit-form__metadata">
+            <div class="app__metadata-row">
+              <span class="app__metadata-lbl">
+                {{ t('metadata-1-lbl') }}
+              </span>
+              <span class="app__metadata-value">
+                <span class="app__price">
+                  {{
+                    formatAmount(erc20.totalSupply.value, erc20.decimals.value)
+                  }}
+                  <span class="app__price-asset">
+                    {{ erc20.symbol.value }}
+                  </span>
+                </span>
+              </span>
+            </div>
+            <div class="app__metadata-row">
+              <span class="app__metadata-lbl">
+                {{ t('metadata-2-lbl') }}
+              </span>
+              <span class="app__metadata-value">
+                {{ `${erc20.name.value} (${erc20.symbol.value})` }}
+              </span>
+            </div>
+            <div class="app__metadata-row">
+              <span class="app__metadata-lbl">
+                {{ t('metadata-3-lbl') }}
+              </span>
+              <span class="app__metadata-value">
+                <app-button
+                  scheme="default"
+                  color="secondary"
+                  size="default"
+                  :text="cropAddress(erc20.address.value)"
+                  @click="copyToClipboard(erc20.address.value)"
+                />
+              </span>
+            </div>
+            <div class="app__metadata-row">
+              <span class="app__metadata-lbl">
+                {{ t('metadata-4-lbl') }}
+              </span>
+              <span class="app__metadata-value">
+                <app-button
+                  scheme="default"
+                  color="secondary"
+                  size="default"
+                  :text="cropAddress(erc20.owner.value)"
+                  :icon-right="$icons.duplicate"
+                  @click="copyToClipboard(erc20.owner.value)"
+                />
+              </span>
+            </div>
+            <div class="app__metadata-row">
+              <span class="app__metadata-lbl">
+                {{ t('metadata-5-lbl') }}
+              </span>
+              <span class="app__metadata-value">
+                {{ erc20.decimals.value }}
+              </span>
+            </div>
+            <div class="app__metadata-row">
+              <span class="app__metadata-lbl">
+                {{ t('metadata-6-lbl') }}
+              </span>
+              <span class="app__metadata-value">
+                <span class="app__price">
+                  {{ formatAmount(accountBalance, erc20.decimals.value) }}
+                  <span class="app__price-asset">
+                    {{ erc20.symbol.value }}
+                  </span>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </app-block>
+    </div>
+    <div class="erc20-edit-form__block-wrp">
+      <h3 class="app__module-block-title">
+        {{ t('block-title-2') }}
+      </h3>
+      <tabs
+        v-model="currentTabNumber"
+        :tabs-data="[
+          { title: 'Read', number: 1 },
+          { title: 'Write', number: 2 },
+        ]"
+      />
+      <app-block>
+        <div class="app__module-content">
+          <common-form
+            :form-scheme="formScheme"
+            class="erc20-edit-form__form-control"
+            @submit="handleApprove"
+          />
+        </div>
+      </app-block>
+    </div>
+  </div>
+</template>
+
+<!-- <template>
   <div class="erc20-edit-form">
     <div class="app__module-title-wrp">
       <app-button
@@ -325,7 +449,7 @@ const formScheme = {
       </app-block>
     </div>
   </div>
-</template>
+</template> -->
 
 <style lang="scss" scoped>
 .erc20-edit-form {
