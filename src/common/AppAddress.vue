bd5c7ca1e295d7559476bd2a474adf68d3744f59
Nazar Revom <shevanzr@gmail.com>
2022-11-24 14:40:41 +0100

    Added AppAddress, refactored addresess to AppAddress, little fixes

---
+++ src/common/AppAddress.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { useWeb3ProvidersStore } from '@/store'
+import { Icon } from '@/common'
+import { cropAddress, copyToClipboard } from '@/helpers'
+
+const { provider } = storeToRefs(useWeb3ProvidersStore())
+
+withDefaults(
+  defineProps<{
+    address: string
+    href?: string
+    isCropped?: boolean
+    isCopyable?: boolean
+  }>(),
+  {
+    isCropped: true,
+    isCopyable: true,
+    href: '',
+  },
+)
+</script>
+
+<template>
+  <div class="app-address app__link-wrp" :title="address">
+    <a
+      class="app__link"
+      :href="href ? href : provider.getAddressUrl(address)"
+      target="_blank"
+      v-bind="$attrs"
+    >
+      {{ isCropped ? cropAddress(address) : address }}
+    </a>
+    <button
+      v-if="isCopyable"
+      class="app__link-icon-wrp"
+      @click="copyToClipboard(address)"
+    >
+      <icon class="app__link-icon" :name="$icons.duplicateFilled" />
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
