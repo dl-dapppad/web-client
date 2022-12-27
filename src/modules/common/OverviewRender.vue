ddd7ee64522df66bdbb80b5d690fbdb063fc1e13
Nazar <revom@pop-os.localdomain>
2022-12-27 16:08:47 +0100

    Break words at OverviewRender

--- src/modules/common/OverviewRender.vue
+++ src/modules/common/OverviewRender.vue
@@ -22,14 +22,14 @@ withDefaults(
       :key="row.name"
       class="app__metadata-row app__metadata-row--mobile-break-line"
     >
-      <span class="app__metadata-lbl">
+      <span class="app__metadata-lbl overview-render__key">
         {{ row.name }}
       </span>
       <span v-if="isLoaded" class="app__metadata-value">
         <template v-if="row.type === OVERVIEW_ROW.amount">
-          <span class="app__price">
+          <span class="app__price overview-render__value">
             {{ row.value.split(' ')[0] }}
-            <span class="app__price-asset">
+            <span class="app__price-asset overview-render__value">
               {{ row.value.split(' ')[1] }}
             </span>
           </span>
@@ -46,7 +46,9 @@ withDefaults(
           />
         </template>
         <template v-else>
-          {{ row.value }}
+          <span class="overview-render__value">
+            {{ row.value }}
+          </span>
         </template>
       </span>
       <loader v-else />
@@ -54,4 +56,12 @@ withDefaults(
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.overview-render__key {
+  padding-right: toRem(50);
+}
+
+.overview-render__value {
+  word-break: break-all;
+}
+</style>
